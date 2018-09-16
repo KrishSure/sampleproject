babb3195a18966c3db5362630feff20a412edd30
Venkata Krishna Rao Sure <sure.venkata.krishna@gmail.com>
2018-09-16 07:54:07 +0530

    Initial project commit

---
+++ sampleproject/src/main/java/org/maven/project/sampleproject/javademo/SecondProgram.java
@@ -0,0 +1,10 @@
+package org.maven.project.sampleproject.javademo;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" We are in Second Programe");
+	}
+
+}
---
+++ sampleproject/src/main/java/org/maven/project/sampleproject/selenium/FirstSeleniumPrograme.java
@@ -0,0 +1,21 @@
+package org.maven.project.sampleproject.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumPrograme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//step 1: downloading the geckodriver.exe file and extracted in below location
+		//step 2: The path of the geckodriver.exe file is set to a property for webdriver.gecko.driver key
+		System.setProperty("webdriver.gecko.driver", "D:\\Softwares\\selenium_info\\geckodriver-v0.22.0-win64\\geckodriver.exe");
+		
+		//step 3: Creating a driver object with FirefoxDriver
+		WebDriver driver = new FirefoxDriver();
+		
+		//step 4: Launching the browser
+		driver.get("https://www.facebook.com/");
+	}
+
+}
